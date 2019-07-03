128969fbf12ca9ef8e7b8c562050181a87113ff4
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-03 05:55:42 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
-function result() {
+
 var q1 = document.querySelector("#All input").value;
-var q2 =document.querySelector("#Pandemic input").value;
-var q3 =document.querySelector("#Smallpox input").value;
+var q2 = document.querySelector("#Pandemic input").value;
+var q3 = document.querySelector("#Smallpox input").value;
 var q4 = document.querySelector("#Neutral input").value;
-var q5 =document.querySelector("#Lungs input").value;
-var numb =0;
-if (q1 == "All") {
-    numb++
-}
-if (q2 == "Pandemic") {
-    numb++
-}
-if (q3 == "Smallpox") {
-    numb++
-}
-if (q4 == "Neutral") {
-    numb++
-}
-if (q5 == "Lungs") {
-    numb++
-}
-    alert ("Congratulation! You got Full marks");
+var q5 = document.querySelector("#Lungs input").value;
+var numb = 0;
+
+
+function result() {
+
+    if (q1 == "All") {
+        numb++
+    }
+    if (q2 == "Pandemic") {
+        numb++
+    }
+    if (q3 == "Smallpox") {
+        numb++
+    }
+    if (q4 == "Neutral") {
+        numb++
+    }
+    if (q5 == "Lungs") {
+
+        numb++
+    }
+    alert("Congratulation! You got Full marks");
 
 }
-// }
-// function result(){
-//     alert("hello");
-// }
+
+
+
